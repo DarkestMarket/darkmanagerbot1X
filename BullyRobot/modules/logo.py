3d47b17de18dc604138a70dd1d3eb55008e4a78f
DarkestMarket <108482327+DarkestMarket@users.noreply.github.com>
2022-07-01 21:00:22 +0530

    Update logo.py

--- BullyRobot/modules/logo.py
+++ BullyRobot/modules/logo.py
@@ -276,18 +276,18 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "fallen.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"ʟᴏɢᴏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ 𝙱𝚄𝙻𝙻𝚈 ✘ ʀᴏʙᴏᴛ")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"ʟᴏɢᴏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Dark Controller")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
  except Exception as e:
-    await event.reply(f'ғʟᴏᴏᴅᴡᴀɪᴛ ᴇʀʀᴏʀ, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @BullyxSupport')
+    await event.reply(f'ғʟᴏᴏᴅᴡᴀɪᴛ ᴇʀʀᴏʀ, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @DarkestMarket666')
 
 
 __mod_name__ = "Lᴏɢᴏ​"
 
 __help__ = """
-Bully Robot can create some beautiful and attractive logo for your profile pics.
+Dark Controller can create some beautiful and attractive logo for your profile pics.
 
 ❍ /logo (Text) *:* Create a logo of your given text with random view.
 """

5d72615156d058528e1b979bbfb0d4b264277940
DarkestMarket <108482327+DarkestMarket@users.noreply.github.com>
2022-07-01 21:01:52 +0530

    Update alive.py

--- BullyRobot/modules/alive.py
+++ BullyRobot/modules/alive.py
@@ -18,11 +18,11 @@ PHOTO = [
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ 𝙱𝚄𝙻𝙻𝚈 ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗕𝗨𝗟𝗟𝗬](https://t.me/jehrilla_cockroach)** \n\n"
+  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [Suryansh](https://t.me/Suryansh_Pandit1)** \n\n"
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/bullyxguardianbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/BullyxSupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/darkmanagerXXbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/DarkestXmarket666")]]
   ran = random.choice(PHOTO)
   await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
 
